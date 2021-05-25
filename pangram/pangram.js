7ec2f039cbbb584c5a22d0c1cb9385ec9eb5b098
Yova21 <74622754+Yova21@users.noreply.github.com>
2021-05-25 17:12:10 -0500

    pangram

---
+++ pangram/pangram.js
@@ -0,0 +1,9 @@
+//
+// This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const isPangram = (entrada) => {
+    const lowered = entrada.toLowerCase()
+    return 'abcdefghijklmnopqrstuvwxyz'.split('').every((lett) => lowered.includes(lett))
+}
